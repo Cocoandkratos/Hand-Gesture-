5927a5dfa2e55144771a06011071cde4705c9236
Cocoandkratos <81406537+Cocoandkratos@users.noreply.github.com>
2021-12-10 18:41:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -36,3 +36,85 @@ function speak(){
     var utterthis = new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
     synth.speak(utterthis);
 }
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+    
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log("Aaniya Khan");
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+
+        if(results[0].label == "Good")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+
+        if(results[0].label == "Bad")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128078;";
+        }
+
+        if(results[0].label == "Right")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128073;"
+        }
+
+        
+        if(results[0].label == "Victory")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#9996;"
+        }
+
+        
+        if(results[0].label == "Bye")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128075;"
+        }
+
+
+        if(results[0].label == "Good")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+
+        if(results[0].label == "Bad")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128078;";
+        }
+
+        if(results[0].label == "Right")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128073;"
+        }
+
+        
+        if(results[0].label == "Victory")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#9996;"
+        }
+
+        
+        if(results[0].label == "Bye")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128075;"
+        }
+
+
+        
+    }
+    
+}
+

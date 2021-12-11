36003ce400124902a0d2d2047edaf39372f229c8
Cocoandkratos <81406537+Cocoandkratos@users.noreply.github.com>
2021-12-11 09:05:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ Webcam.attach('#camera');
 function take_snapshot()
 {
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML = '<img id="capture_image" src = "'+data_uri+'"/>';
+        document.getElementById("result").innerHTML = '<img id="captured_image" src = "'+data_uri+'"/>';
     })
 }
 
